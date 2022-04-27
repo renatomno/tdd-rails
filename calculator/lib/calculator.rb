289d7955a9b3efb3e86bf7c3ded7dae623db0ebc
Renato <xrenatomno@gmail.com>
2022-04-27 04:24:36 -0300

    hello world! primeiro commit

---
+++ calculator/lib/calculator.rb
@@ -0,0 +1,6 @@
+class Calculator
+    def sum(num1, num2)
+        num1 + num2
+    end
+    
+end
---
+++ calculator/spec/calculator/calculator_spec.rb
@@ -0,0 +1,22 @@
+require "calculator"
+
+describe Calculator do
+    
+
+    context '#sum' do
+        it 'with positive numbers' do
+            result = subject.sum(5, 8)
+            expect(result).to eq(13) 
+        end
+        
+        it 'with negative numbers' do
+            result = subject.sum(-3, -5)
+            expect(result).to eq(-8)
+        end
+
+        it 'with negative and positive numbers' do
+            result = subject.sum(4, -2)
+            expect(result).to eq(2)
+        end
+    end
+end
