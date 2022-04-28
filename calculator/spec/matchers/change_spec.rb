b3c99658408e36f8d22713f263b72b590e9212e7
Renato <xrenatomno@gmail.com>
2022-04-28 08:24:38 -0300

    add new-app (testing rspec with rails)

--- calculator/spec/matchers/change_spec.rb
+++ calculator/spec/matchers/change_spec.rb
@@ -5,3 +5,17 @@ describe 'Matcher change' do
     it {expect{Contador.incrementa}.to change { Contador.qtd}.by(1)}
     it {expect{Contador.incrementa}.to change { Contador.qtd}.from(2).to(3)}
 end
+
+
+describe 'Mocks' do
+    it '#bar' do
+        # setup
+        student = Student.new
+
+        # verify
+        expect(student).to receive(:bar)
+
+        # exercise
+        student.bar
+    end
+end
