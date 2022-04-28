3e68ea54677b32ef012ca241ebac59aec59e4b5b
Renato <xrenatomno@gmail.com>
2022-04-28 01:23:35 -0300

    add Hooks (before e after)

--- calculator/spec/matchers/atributos_spec.rb
+++ calculator/spec/matchers/atributos_spec.rb
@@ -3,6 +3,23 @@ require 'pessoa'
 describe 'Matchers de atributos' do
     subject(:pessoa) {Pessoa.new()}
     
+    config.before(:suite) do
+        puts "Isso aqui aparece 1x, antes dos testes começar"
+    end
+    
+    config.after(:suite) do
+        puts "Isso aqui aparece 1x, depois dos testes serem feitos"
+    end
+    
+    config.before(:all) do
+        puts "Isso aqui aparece antes de cada teste"
+    end
+    
+    config.after(:all) do
+        puts "Isso aqui aparece depois de cada teste"
+    end
+    
+    
     it 'have_attributes' do
         pessoa.nome = 'Renato'
         pessoa.idade = 26
--- calculator/spec/spec_helper.rb
+++ calculator/spec/spec_helper.rb
@@ -13,7 +13,16 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    puts "vou testaaaaaaar"
+  end
+
+  config.after(:suite) do
+    puts "testei"
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
